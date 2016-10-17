24dd27b01a3ace262e86f025a378a3d8fbb17e1d
Almaz <diamond.scalping@yandex.com>
2016-10-17 17:43:48 +0500

    v0.0.0.1

---
+++ class.wp_settings_theme.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class WP_SettingsTheme - Settings and Management for Templates
+ *
+ * Creates a page with the settings for the installed WordPress theme.
+ * A very useful tool for developers who work with WordPress.
+ *
+ * @link    https://github.com/DevDiamondCom/WP_SettingsTheme
+ * @version 0.0.0.1
+ * @author  DevDiamond <me@devdiamond.com>
+ * @license GPLv3 or later - http://www.gnu.org/licenses/gpl-3.0.html
+ */
+class WP_SettingsTheme
+{
+	/**
+	 * Constructor
+	 */
+	public function __construct(){}
+}
